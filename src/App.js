e8dcc52c04ae615bcb5300f732b323fa2a64b164
margaret-ef <107960570+margaret-ef@users.noreply.github.com>
2022-08-09 05:58:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./input.css";
+import Corescore from "./components/Corescore";
+
+function App() {
+  return (
+    <div className="">
+      <Corescore />
+    </div>
+  );
+}
+
+export default App;
